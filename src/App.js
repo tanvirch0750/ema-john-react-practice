50fede66e96725aea7ba9d4f8c899aa1e1243b15
tanvirch0750 <tanvir0750@yahoo.com>
2022-03-23 01:12:32 +0600

    implemented localstorage functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { addToDb, getStoredCart } from './utilities/fakedb';
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+ 
   
   useEffect(()=> {
     fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
@@ -16,12 +17,33 @@ function App() {
   }, []);
 
   useEffect(() => {
-    getStoredCart()
-  },[])
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for(const id in storedCart){
+      const addedProduct = products.find(product => product.id === id);
+      if(addedProduct){
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    
+    setCart(savedCart)
+  },[products])
 
-  const handleAddToCart = (product) => {
-    setCart([...cart, product]);
-    addToDb(product.id)
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find(product => product.id === selectedProduct.id);
+    if(!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct]
+    } else {
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1
+      newCart = [...rest, exists]
+    }
+    setCart(newCart);
+    addToDb(selectedProduct.id)
   }
 
   return (
--- src/components/Order-Summary/OrderSummary.jsx
+++ src/components/Order-Summary/OrderSummary.jsx
@@ -3,15 +3,21 @@ import { FaArrowRight, FaTrashAlt } from "react-icons/fa";
 import './OrderSummary.css';
 
 const OrderSummary = ({cart}) => {
+   const totalPrice = cart.reduce((acc, currentValue) => acc + currentValue.price * currentValue.quantity, 0);
+   const shippingPrice = cart.reduce((acc, currentValue) => acc + currentValue.shipping, 0);
+   const quantity = cart.reduce((acc, currentValue) => acc + currentValue.quantity , 0);
+  
+   const tax = totalPrice * 0.1;
+   const grandTotal = totalPrice + shippingPrice + tax;
    return (
       <aside className="product-summary">
         <h2>Order Summary</h2>
         <div className="summary-details">
-          <p>Selected Items: {cart.length}</p>
-          <p>Total Price: $1146</p>
-          <p>Total Shipping Charge: $5</p>
-          <p>Tax: $114</p>
-          <h3>Grand Total: $1559</h3>
+          <p>Selected Items: {quantity}</p>
+          <p>Total Price: ${totalPrice}</p>
+          <p>Total Shipping Charge: ${shippingPrice}</p>
+          <p>Tax: ${tax.toFixed(2)}</p>
+          <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
         </div>
         <div className="order-summary__buttons">
           <button className="btn clear-cart-btn">
